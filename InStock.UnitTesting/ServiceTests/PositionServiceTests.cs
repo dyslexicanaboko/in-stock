50f048b5f5a7985fde950a4b2573b89c683d358f
Eli Hayon <dyslexicanaboko@gmail.com>
2023-06-25 23:21:35 -0400

    #14 #15 - Had to fix two unit tests to include the newly added validation classes.

--- InStock.UnitTesting/ServiceTests/PositionServiceTests.cs
+++ InStock.UnitTesting/ServiceTests/PositionServiceTests.cs
@@ -3,110 +3,116 @@ using InStock.Lib.DataAccess;
 using InStock.Lib.Entities;
 using InStock.Lib.Exceptions;
 using InStock.Lib.Services;
-using InStock.Lib.Services.Mappers;
+using InStock.Lib.Validation;
 using NUnit.Framework;
 
 namespace InStock.UnitTesting.ServiceTests
 {
-    [TestFixture]
-    public class PositionServiceTests
-        : TestBaseInStock
+  [TestFixture]
+  public class PositionServiceTests
+    : TestBaseInStock
+  {
+    [SetUp]
+    public void Setup()
     {
-        private IStockRepository _repoStock;
-        private IPositionRepository _repoPosition;
-        private IPositionService _service;
+      _repoStock = A.Fake<IStockRepository>();
 
-        [SetUp]
-        public void Setup()
-        {
-            _repoStock = A.Fake<IStockRepository>();
+      _repoPosition = A.Fake<IPositionRepository>();
 
-            _repoPosition = A.Fake<IPositionRepository>();
+      _validation = new PositionValidation();
 
-            _service = new PositionService(_repoPosition, _repoStock);
-        }
-
-        [Test]
-        public void Add_WhenPositionIsUnique_ThenPositionIsInserted()
-        {
-            //Arrange
-            A.CallTo(() => _repoStock.Select(A<IList<int>>._)).Returns(new List<StockEntity> { GetSomeStock() });
-            A.CallTo(() => _repoPosition.Select(A<int>._, A<string>._)).Returns(Enumerable.Empty<PositionEntity>());
-
-            //Act
-            var actual = _service.Add(GetSomePosition());
-
-            //Assert
-            Assert.AreEqual(1, actual.Count);
-
-            var a = actual.Single();
-
-            Assert.IsTrue(a.IsSuccessful);
-
-            A.CallTo(() => _repoPosition.Insert(A<PositionEntity>._))
-                .MustHaveHappened();
-        }
-
-        [Test]
-        public void Add_WhenPositionIsNotUnique_ThenPositionExistsAlreadyException()
-        {
-            //Arrange
-            A.CallTo(() => _repoStock.Select(A<IList<int>>._)).Returns(new List<StockEntity> { GetSomeStock() });
-            A.CallTo(() => _repoPosition.Select(A<int>._, A<string>._)).Returns(GetSomePositions());
-
-            var position = GetSomePosition();
-
-            //Act/Assert
-            var actual = _service.Add(position);
-
-            //Assert
-            Assert.AreEqual(1, actual.Count);
-
-            var a = actual.Single();
-
-            Assert.IsFalse(a.IsSuccessful);
-            Assert.IsTrue(a.Exception is PositionExistsAlreadyException);
-
-            A.CallTo(() => _repoPosition.Insert(A<PositionEntity>._))
-                .MustNotHaveHappened();
-        }
-
-        [Test]
-        public void Add_WhenStockIsNotFound_ThenStockNotFoundExceptionIsRaised()
-        {
-            //Arrange
-            A.CallTo(() => _repoStock.Select(A<int>._)).Returns(null);
-
-            var position = GetSomePosition();
-
-            //Act/Assert
-            var actual = _service.Add(position);
-
-            //Assert
-            Assert.AreEqual(1, actual.Count);
-            
-            var a = actual.Single();
-
-            Assert.IsFalse(a.IsSuccessful);
-            AssertIsStockIdNotFoundException(a.Exception);
-
-            A.CallTo(() => _repoPosition.Insert(A<PositionEntity>._))
-                .MustNotHaveHappened();
-        }
-
-        [Test]
-        public void Delete_WhenSymbolCannotBeFound_ThenSymbolNotFoundExceptionIsRaised()
-        {
-            //Arrange
-            A.CallTo(() => _repoStock.Select(A<string>._)).Returns(null);
-
-            //Act
-
-            AssertThrowsSymbolNotFoundException(() => _service.Delete(SomeUserId, SomeSymbol));
-            
-            //Assert
-            A.CallTo(() => _repoPosition.Delete(A<int>._, A<string>._))
-                .MustNotHaveHappened();
-        }
+      _service = new PositionService(_repoPosition, _repoStock, _validation);
     }
+
+    private IStockRepository _repoStock;
+
+    private IPositionRepository _repoPosition;
+
+    private IPositionService _service;
+
+    private IPositionValidation _validation;
+
+    [Test]
+    public void Add_WhenPositionIsUnique_ThenPositionIsInserted()
+    {
+      //Arrange
+      A.CallTo(() => _repoStock.Select(A<IList<int>>._)).Returns(new List<StockEntity> { GetSomeStock() });
+      A.CallTo(() => _repoPosition.Select(A<int>._, A<string>._)).Returns(Enumerable.Empty<PositionEntity>());
+
+      //Act
+      var actual = _service.Add(GetSomePosition());
+
+      //Assert
+      Assert.AreEqual(1, actual.Count);
+
+      var a = actual.Single();
+
+      Assert.IsTrue(a.IsSuccessful);
+
+      A.CallTo(() => _repoPosition.Insert(A<PositionEntity>._))
+        .MustHaveHappened();
+    }
+
+    [Test]
+    public void Add_WhenPositionIsNotUnique_ThenPositionExistsAlreadyException()
+    {
+      //Arrange
+      A.CallTo(() => _repoStock.Select(A<IList<int>>._)).Returns(new List<StockEntity> { GetSomeStock() });
+      A.CallTo(() => _repoPosition.Select(A<int>._, A<string>._)).Returns(GetSomePositions());
+
+      var position = GetSomePosition();
+
+      //Act/Assert
+      var actual = _service.Add(position);
+
+      //Assert
+      Assert.AreEqual(1, actual.Count);
+
+      var a = actual.Single();
+
+      Assert.IsFalse(a.IsSuccessful);
+      Assert.IsTrue(a.Exception is PositionExistsAlreadyException);
+
+      A.CallTo(() => _repoPosition.Insert(A<PositionEntity>._))
+        .MustNotHaveHappened();
+    }
+
+    [Test]
+    public void Add_WhenStockIsNotFound_ThenStockNotFoundExceptionIsRaised()
+    {
+      //Arrange
+      A.CallTo(() => _repoStock.Select(A<int>._)).Returns(null);
+
+      var position = GetSomePosition();
+
+      //Act/Assert
+      var actual = _service.Add(position);
+
+      //Assert
+      Assert.AreEqual(1, actual.Count);
+
+      var a = actual.Single();
+
+      Assert.IsFalse(a.IsSuccessful);
+      AssertIsStockIdNotFoundException(a.Exception);
+
+      A.CallTo(() => _repoPosition.Insert(A<PositionEntity>._))
+        .MustNotHaveHappened();
+    }
+
+    [Test]
+    public void Delete_WhenSymbolCannotBeFound_ThenSymbolNotFoundExceptionIsRaised()
+    {
+      //Arrange
+      A.CallTo(() => _repoStock.Select(A<string>._)).Returns(null);
+
+      //Act
+
+      AssertThrowsSymbolNotFoundException(() => _service.Delete(SomeUserId, SomeSymbol));
+
+      //Assert
+      A.CallTo(() => _repoPosition.Delete(A<int>._, A<string>._))
+        .MustNotHaveHappened();
+    }
+  }
 }
--- InStock.UnitTesting/ServiceTests/TradeServiceTests.cs
+++ InStock.UnitTesting/ServiceTests/TradeServiceTests.cs
@@ -3,110 +3,116 @@ using InStock.Lib.DataAccess;
 using InStock.Lib.Entities;
 using InStock.Lib.Exceptions;
 using InStock.Lib.Services;
-using InStock.Lib.Services.Mappers;
+using InStock.Lib.Validation;
 using NUnit.Framework;
 
 namespace InStock.UnitTesting.ServiceTests
 {
-    [TestFixture]
-    public class TradeServiceTests
-        : TestBaseInStock
+  [TestFixture]
+  public class TradeServiceTests
+    : TestBaseInStock
+  {
+    [SetUp]
+    public void Setup()
     {
-        private IStockRepository _repoStock;
-        private ITradeRepository _repoTrade;
-        private ITradeService _service;
+      _repoStock = A.Fake<IStockRepository>();
 
-        [SetUp]
-        public void Setup()
-        {
-            _repoStock = A.Fake<IStockRepository>();
+      _repoTrade = A.Fake<ITradeRepository>();
 
-            _repoTrade = A.Fake<ITradeRepository>();
+      _validation = new TradeValidation();
 
-            _service = new TradeService(_repoTrade, _repoStock);
-        }
-
-        [Test]
-        public void Add_WhenTradeIsUnique_ThenTradeIsInserted()
-        {
-            //Arrange
-            A.CallTo(() => _repoStock.Select(A<IList<int>>._)).Returns(new List<StockEntity> { GetSomeStock() });
-            A.CallTo(() => _repoTrade.Select(A<int>._, A<string>._)).Returns(Enumerable.Empty<TradeEntity>());
-
-            //Act
-            var actual = _service.Add(GetSomeTrade());
-
-            //Assert
-            Assert.AreEqual(1, actual.Count);
-
-            var a = actual.Single();
-
-            Assert.IsTrue(a.IsSuccessful);
-
-            A.CallTo(() => _repoTrade.Insert(A<TradeEntity>._))
-                .MustHaveHappened();
-        }
-
-        [Test]
-        public void Add_WhenTradeIsNotUnique_ThenTradeExistsAlreadyException()
-        {
-            //Arrange
-            A.CallTo(() => _repoStock.Select(A<IList<int>>._)).Returns(new List<StockEntity> { GetSomeStock() });
-            A.CallTo(() => _repoTrade.Select(A<int>._, A<string>._)).Returns(GetSomeTrades());
-
-            var position = GetSomeTrade();
-
-            //Act/Assert
-            var actual = _service.Add(position);
-
-            //Assert
-            Assert.AreEqual(1, actual.Count);
-
-            var a = actual.Single();
-
-            Assert.IsFalse(a.IsSuccessful);
-            Assert.IsTrue(a.Exception is TradeExistsAlreadyException);
-
-            A.CallTo(() => _repoTrade.Insert(A<TradeEntity>._))
-                .MustNotHaveHappened();
-        }
-
-        [Test]
-        public void Add_WhenStockIsNotFound_ThenStockNotFoundExceptionIsRaised()
-        {
-            //Arrange
-            A.CallTo(() => _repoStock.Select(A<int>._)).Returns(null);
-
-            var position = GetSomeTrade();
-
-            //Act/Assert
-            var actual = _service.Add(position);
-
-            //Assert
-            Assert.AreEqual(1, actual.Count);
-            
-            var a = actual.Single();
-
-            Assert.IsFalse(a.IsSuccessful);
-            AssertIsStockIdNotFoundException(a.Exception);
-
-            A.CallTo(() => _repoTrade.Insert(A<TradeEntity>._))
-                .MustNotHaveHappened();
-        }
-
-        [Test]
-        public void Delete_WhenSymbolCannotBeFound_ThenSymbolNotFoundExceptionIsRaised()
-        {
-            //Arrange
-            A.CallTo(() => _repoStock.Select(A<string>._)).Returns(null);
-
-            //Act
-
-            AssertThrowsSymbolNotFoundException(() => _service.Delete(SomeUserId, SomeSymbol));
-            
-            //Assert
-            A.CallTo(() => _repoTrade.Delete(A<int>._, A<string>._))
-                .MustNotHaveHappened();
-        }
+      _service = new TradeService(_repoTrade, _repoStock, _validation);
     }
+
+    private IStockRepository _repoStock;
+
+    private ITradeRepository _repoTrade;
+
+    private ITradeService _service;
+
+    private ITradeValidation _validation;
+
+    [Test]
+    public void Add_WhenTradeIsUnique_ThenTradeIsInserted()
+    {
+      //Arrange
+      A.CallTo(() => _repoStock.Select(A<IList<int>>._)).Returns(new List<StockEntity> { GetSomeStock() });
+      A.CallTo(() => _repoTrade.Select(A<int>._, A<string>._)).Returns(Enumerable.Empty<TradeEntity>());
+
+      //Act
+      var actual = _service.Add(GetSomeTrade());
+
+      //Assert
+      Assert.AreEqual(1, actual.Count);
+
+      var a = actual.Single();
+
+      Assert.IsTrue(a.IsSuccessful);
+
+      A.CallTo(() => _repoTrade.Insert(A<TradeEntity>._))
+        .MustHaveHappened();
+    }
+
+    [Test]
+    public void Add_WhenTradeIsNotUnique_ThenTradeExistsAlreadyException()
+    {
+      //Arrange
+      A.CallTo(() => _repoStock.Select(A<IList<int>>._)).Returns(new List<StockEntity> { GetSomeStock() });
+      A.CallTo(() => _repoTrade.Select(A<int>._, A<string>._)).Returns(GetSomeTrades());
+
+      var position = GetSomeTrade();
+
+      //Act/Assert
+      var actual = _service.Add(position);
+
+      //Assert
+      Assert.AreEqual(1, actual.Count);
+
+      var a = actual.Single();
+
+      Assert.IsFalse(a.IsSuccessful);
+      Assert.IsTrue(a.Exception is TradeExistsAlreadyException);
+
+      A.CallTo(() => _repoTrade.Insert(A<TradeEntity>._))
+        .MustNotHaveHappened();
+    }
+
+    [Test]
+    public void Add_WhenStockIsNotFound_ThenStockNotFoundExceptionIsRaised()
+    {
+      //Arrange
+      A.CallTo(() => _repoStock.Select(A<int>._)).Returns(null);
+
+      var position = GetSomeTrade();
+
+      //Act/Assert
+      var actual = _service.Add(position);
+
+      //Assert
+      Assert.AreEqual(1, actual.Count);
+
+      var a = actual.Single();
+
+      Assert.IsFalse(a.IsSuccessful);
+      AssertIsStockIdNotFoundException(a.Exception);
+
+      A.CallTo(() => _repoTrade.Insert(A<TradeEntity>._))
+        .MustNotHaveHappened();
+    }
+
+    [Test]
+    public void Delete_WhenSymbolCannotBeFound_ThenSymbolNotFoundExceptionIsRaised()
+    {
+      //Arrange
+      A.CallTo(() => _repoStock.Select(A<string>._)).Returns(null);
+
+      //Act
+
+      AssertThrowsSymbolNotFoundException(() => _service.Delete(SomeUserId, SomeSymbol));
+
+      //Assert
+      A.CallTo(() => _repoTrade.Delete(A<int>._, A<string>._))
+        .MustNotHaveHappened();
+    }
+  }
 }
