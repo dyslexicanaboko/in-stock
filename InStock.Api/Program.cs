d643a91f8a5f2f00accb32ec57990279ac047838
Eli Hayon <dyslexicanaboko@gmail.com>
2023-07-21 20:20:39 -0400

    #20 Enable CORS as expected

--- InStock.Api/Program.cs
+++ InStock.Api/Program.cs
@@ -1,57 +1,81 @@
-using InStock.Api;
+using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace InStock.Api;
+
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-ContainerConfig.Configure(builder.Host);
+    ContainerConfig.Configure(builder.Host);
 
 //https://www.c-sharpcorner.com/article/how-to-implement-jwt-authentication-in-web-api-using-net-6-0-asp-net-core/
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-{
-  options.RequireHttpsMetadata = false;
-  options.SaveToken = true;
-  options.TokenValidationParameters = new TokenValidationParameters()
-  {
-    ValidateIssuer = true,
-    ValidateAudience = true,
-    ValidAudience = builder.Configuration["Jwt:Audience"],
-    ValidIssuer = builder.Configuration["Jwt:Issuer"],
-    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-  };
-});
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+      .AddJwtBearer(
+        options =>
+        {
+          options.RequireHttpsMetadata = false;
+          options.SaveToken = true;
+
+          options.TokenValidationParameters = new TokenValidationParameters()
+          {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+          };
+        });
 
 //https://stackoverflow.com/questions/70554844/asp-net-core-6-web-api-making-fields-required
 //The controller was making non-nullable properties required without my permission
-builder.Services
-    .AddControllers(options => options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true)
-    .AddNewtonsoftJson();
+    builder.Services
+      .AddControllers(options => options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true)
+      .AddNewtonsoftJson();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+    var app = builder.Build();
 
-app.Logger.LogInformation("InStock API is running");
+    app.Logger.LogInformation("InStock API is running");
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    if (app.Environment.IsDevelopment())
+    {
+      app.UseSwagger();
+      app.UseSwaggerUI();
+      
+      SetLooseCorsPolicyForDevelopmentPurposesOnly(app);
+    }
+
+    //Because Next.js sucks I am disabling https for now
+    //app.UseHttpsRedirection();
+    
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.UseMiddleware<ResponseMiddleware>(app.Logger);
+
+    app.Run();
+  }
+
+  private static void SetLooseCorsPolicyForDevelopmentPurposesOnly(IApplicationBuilder app)
+  {
+    app.UseCors(
+      policy =>
+      {
+        policy.AllowAnyOrigin();
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+      });
+  }
 }
-
-//Because Next.js sucks I am disabling https for now
-//app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.UseMiddleware<ResponseMiddleware>(app.Logger);
-
-app.Run();
