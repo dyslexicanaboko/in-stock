3b19c2f8d0550a4a80fff4d5e789aa753b7a134a
Eli Hayon <dyslexicanaboko@gmail.com>
2023-08-02 02:54:10 -0400

    #20 There is a problem with the YahooApi right now and it has been reported. Just waiting for someone to take a look at it. In the mean time I am catching the exceptions and just logging them so that the portfolio can still render.

--- InStock.Lib/Services/PortfolioService.cs
+++ InStock.Lib/Services/PortfolioService.cs
@@ -1,5 +1,7 @@
 ﻿using InStock.Lib.DataAccess;
+using InStock.Lib.Entities;
 using InStock.Lib.Entities.Composites;
+using Microsoft.Extensions.Logging;
 
 namespace InStock.Lib.Services
 {
@@ -7,14 +9,16 @@ namespace InStock.Lib.Services
     : IPortfolioService
   {
     private readonly IPortfolioRepository _repoPortfolio;
-
     //I don't like injecting a Service into a Service, but this is an edge case
     private readonly IQuoteService _quoteService;
+    private readonly ILogger<PortfolioService> _logger;
 
     public PortfolioService(
+      ILogger<PortfolioService> logger,
       IPortfolioRepository repoPortfolio,
       IQuoteService quoteService)
     {
+      _logger = logger;
       _repoPortfolio = repoPortfolio;
       _quoteService = quoteService;
     }
@@ -29,17 +33,36 @@ namespace InStock.Lib.Services
       //Cache is being utilized internally to guard against spamming the YahooApi and the DB
       foreach (var p in lst)
       {
-        var quote = await _quoteService.Add(p.StockId, p.Symbol);
+        QuoteEntity quote;
+
+        try
+        {
+          //The YahooApi can throw stupid unexpected exceptions sometimes
+          quote = await _quoteService.Add(p.StockId, p.Symbol);
+        }
+        catch (Exception ex)
+        {
+          _logger.LogError(ex, "YahooApi Failure");
+
+          quote = new QuoteEntity();
+        }
 
         p.CurrentPrice = Convert.ToDecimal(quote.Price);
         p.CurrentValue = p.CurrentPrice * p.Shares;
         p.DaysHeld = Convert.ToDecimal((dtm - p.OwnedAsOf).TotalDays);
         p.TotalGain = p.CurrentValue - p.CostBasis;
-        p.TotalGainPercentage = 1M - (p.TotalGain / p.CostBasis);
-        p.GainRate = p.TotalGain / p.DaysHeld;
+        p.TotalGainPercentage = 1M - SafeDivide(p.TotalGain, p.CostBasis);
+        p.GainRate = SafeDivide(p.TotalGain, p.DaysHeld);
       }
 
       return lst;
+    }
+
+    private static decimal SafeDivide(decimal numerator, decimal divisor)
+    {
+      if (divisor == 0) return 0;
+
+      return numerator / divisor;
     }
   }
 }
